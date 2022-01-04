17869e02300a1cd7ebb085972437251e4df9be3e
davidvg <davidvazquez.gijon@gmail.com>
2022-01-04 10:36:25 +0100

    Added some doc

--- 02-LightScheduler/src/RandomMinute.c
+++ 02-LightScheduler/src/RandomMinute.c
@@ -16,4 +16,11 @@ int RandomMinute_GetImpl(void)
     return result;
 }
 
+/**
+ * In production t he function pointer is pointed to the actual function in the
+ * source file. This can be overriden in tests by pointing to another function
+ * that acts as a double for the actual one.
+ * After the test is done the pointer should be restored to the production
+ * function.
+ */
 int (*RandomMinute_Get)(void) = RandomMinute_GetImpl;
